98f4b702687703f96e786fa1e13a9e463d570ee1
HCLzlh <2404700351@qq.com>
2018-11-26 12:44:56 +0800

    del

---
+++ cmake-build-debug/CMakeFiles/argc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/argc.dir/main.cpp.o"
+  "argc.pdb"
+  "argc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/argc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
